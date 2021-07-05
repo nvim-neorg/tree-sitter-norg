bfffda163aec7e9a969cdbad1b66a47f7f30bf90
Vhyrro <vhyrro@gmail.com>
2021-07-05 22:54:47 +0200

    fix: issues with parsing of drawers and some accidental ERRORs

--- grammar.js
+++ grammar.js
@@ -22,7 +22,7 @@ module.exports = grammar({
 		todo_item_pending: $ => seq(optional($._leading_whitespace), /\-\s+\[\s*\*\s*\]\s+/, $.paragraph_segment),
 		todo_item_undone: $ => seq(optional($._leading_whitespace), /\-\s+\[\s+\]\s+/, $.paragraph_segment),
 
-		tag: $ => seq(optional($._leading_whitespace), token(/@/), $.tag_name, repeat(seq(token.immediate('.'), $.tag_name)), choice(seq(/\s+/, seq(choice($.tag_parameters, $._soft_paragraph_break), repeat(seq(/[\t ]+/, $.tag_parameters)), $._soft_paragraph_break)), $._soft_paragraph_break), $.tag_content, '@end'),
+		tag: $ => seq(optional($._leading_whitespace), token.immediate('@'), $.tag_name, repeat(seq(token.immediate('.'), $.tag_name)), choice(seq(/\s+/, seq(choice($.tag_parameters, $._soft_paragraph_break), repeat(seq(/[\t ]+/, $.tag_parameters)), $._soft_paragraph_break)), $._soft_paragraph_break), $.tag_content, '@end'),
 		tag_content: $ => prec.left(1, repeat1(seq(/[^\n]+/, $._soft_paragraph_break))),
 		tag_name: $ => /[a-z_]+/,
 		tag_parameters: $ => /[^\s]+/,
@@ -30,7 +30,7 @@ module.exports = grammar({
 		carryover_tag: $ => seq(optional($._leading_whitespace), token('$'), $.tag_name, repeat(seq(token.immediate('.'), $.tag_name)), choice(seq(/\s+/, seq(choice($.tag_parameters, $._soft_paragraph_break), repeat(seq(/[\t ]+/, $.tag_parameters)), $._soft_paragraph_break)), $._soft_paragraph_break), repeat($._soft_paragraph_break), repeat1($.paragraph)), 
 
 		drawer: $ => seq(optional($._leading_whitespace), /\|{2}\s+/, field("drawer_name", $.paragraph_segment), optional($.drawer_content), '||'),
-		drawer_content: $ => /.+/,
+		drawer_content: $ => repeat1(/.+/),
 
 		paragraph: $ => prec.left(1, seq(repeat1(seq(repeat(choice($.heading1, $.heading2, $.heading3, $.heading4)), prec(1, choice($.paragraph_segment, $.quote, $.marker, $.unordered_list, $.todo_item_done, $.todo_item_pending, $.todo_item_undone, $.tag, $.drawer)))), choice($._eof, $._soft_paragraph_break))),
 
@@ -43,7 +43,7 @@ module.exports = grammar({
 		_attached_modifiers: $ => choice(token.immediate('*'), token.immediate('_'), token.immediate('`'), token.immediate('|'), token.immediate('^'), token.immediate('$')),
 
 		escape_sequence: $ => seq(optional($._leading_whitespace), token.immediate('\\'), token.immediate(/./)),
-		words: $ => seq(choice(/[^\s]/, $._leading_whitespace), repeat1(/[^\n]/)),
+		words: $ => seq(choice(/[^\s]/, $._leading_whitespace), repeat(/[^\n]/)),
 		paragraph_segment: $ => choice(seq($.words, $.trailing_modifier, token.immediate('\n'), $.paragraph_segment), seq($.words, choice($._soft_paragraph_break, $._eof))),
 		trailing_modifier: $ => token.immediate('~'),
 
