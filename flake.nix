72ec2f94fa51d47a8228f6b77fd40d314157873c
bandithedoge <bandithedoge@protonmail.com>
2022-02-11 22:41:23 +0100

    nix: expose generic defaultPackage

--- flake.nix
+++ flake.nix
@@ -8,20 +8,24 @@
     };
   };
   outputs = { self, nixpkgs, flake-utils, ... }@inputs:
-    flake-utils.lib.eachDefaultSystem (system:
-      let pkgs = import nixpkgs { inherit system; }; in
-      {
-        defaultPackage = pkgs.callPackage (nixpkgs + "/pkgs/development/tools/parsing/tree-sitter/grammar.nix") { } {
-          language = "norg";
-          source = ./.;
-          inherit (pkgs.tree-sitter) version;
-        };
-        devShell = pkgs.mkShell {
-          nativeBuildInputs = with pkgs; [
-            nodejs
-            nodePackages.node-gyp
-            tree-sitter
-          ];
-        };
-      });
+    let
+      defaultPackage = pkgs: pkgs.callPackage (nixpkgs + "/pkgs/development/tools/parsing/tree-sitter/grammar.nix") { } {
+        language = "norg";
+        source = ./.;
+        inherit (pkgs.tree-sitter) version;
+      };
+    in
+    (flake-utils.lib.eachDefaultSystem
+      (system:
+        let pkgs = import nixpkgs { inherit system; }; in
+        {
+          defaultPackage = defaultPackage pkgs;
+          devShell = pkgs.mkShell {
+            nativeBuildInputs = with pkgs; [
+              nodejs
+              nodePackages.node-gyp
+              tree-sitter
+            ];
+          };
+        })) // (let pkgs = import nixpkgs { }; in { defaultPackage = defaultPackage pkgs; });
 }
